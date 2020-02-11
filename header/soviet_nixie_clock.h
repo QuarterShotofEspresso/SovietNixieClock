1fd01731b44c806dd334f991bc9362e5395618f9
RB <ratnodeepb@gmail.com>
2020-02-11 14:35:25 -0800

    Need to rethink the structure of all components

--- header/soviet_nixie_clock.h
+++ header/soviet_nixie_clock.h
@@ -4,27 +4,50 @@
 
 /************ CODE MAINTAINING TIME **************/
 
-struct timeData {
-    unsigned char hour, minute, second;
+struct sncTimeData {
+    
     unsigned char timeUnitIndex;
-    unsigned char timeData[3] = {hour, minute, second};
+    unsigned char timeData[3]; // {hour, minute, second} <-- the unitary order
     
     // function selecting which unit of time to adjust
-    void ( * shiftTimeUnit ) ( unsigned char * timeIndex );
-    
+    void ( *shiftUnitaryIndex ) ( unsigned char *unitaryIndex );
+    void ( *setTime ) ( unsigned char unitaryIndex, unsigned char value );
 }
 
 
-void shiftTimeUnit ( unsigned char * timeIndex ) {
-    *timeUnitIndex = ( *timeUnitIndex + 1 ) % 3;
-}
+void shiftUnitaryIndex ( unsigned char *unitaryIndex ); // pass by reference so to change the value of the original variable
+void setTime ( unsigned char unitaryIndex, unsigned char value ); // this function sets the time to a specifiable value
 
 ///////////////////////////////////////////////////
 
 
 
 
+/************ CODE MAINTAINING DISPLAY **************/
+
+void startDisplay ( void ); // sets the display to the current data loaded into timeData;
+void stopDisplay ( unsigned char segment ); // turns off the display
+
+//////////////////////////////////////////////////////
 
 
+
+
+/************ CODE MAINTAINING RTC **************/
+
+void saveTime ( struct sncTimeData *time );
+void recallTime ( struct sncTimeData *time );
+
+///////////////////////////////////////////////////
+
+
+
+
+/************ TICK FUNCTION DECLARATIONS ************/
+
+void tick_timer ( void );
+void tick_buttons ( void );
+
+//////////////////////////////////////////////////////
 
 #endif /* _SOVIET_NIXIE_CLOCK */
--- source/main.c
+++ source/main.c
@@ -1,20 +1,54 @@
-#include "simAVRHeader.h"
+//#include "simAVRHeader.h" // not including simulator
 #include "soviet_nixie_clock.h"
 
 // global variables
-enum state_TimeIncrementer {START, INIT, COUNT, TIME_INC} state_TIMER;
-enum state_buttonSignals {STATE, IDLE, INC_WAIT, MANUAL_INC, SHIFT_LEFT, SHIFT_LEFT_WAIT, RESET_WAIT, SAVE_TIME, RESET, DECREMENT_WAIT, SHIFT_RIGHT, MANUAL_INC} state_BUTTON;
+enum state_TimeIncrementer {START_timer, COUNT, TIME_INC} state_TIMER;
+enum state_buttonSignals {START_button, INIT, IDLE, INC_WAIT, MAN_INC, SHIFT_LEFT, RESET_WAIT, SAVE_TIME, RESET, DECREMENT_WAIT, SHIFT_RIGHT, MAN_INC, MAST_WAIT} state_BUTTON;
 
 unsigned char heldFor = 0, timerCount = 0;
+unsigned char A0 = 0, A1 = 0;
+
+// defines
+#define HELD_FOR_THRESHOLD 10
+
+// additional macros
+#define unt ( !A0 && !A1 )
+#define bot ( A0 && A1 )
+#define inc ( A0 && !A1 )
+#define dec ( !A0 && A1 )
+#define HREQ ( heldFor >= HELD_FOR_THRESHOLD )
+
 
 // additional functions
+void samplePins ( void );
 
-
-
-
+/*************  MAIN FUNCTION  **************/
 int main ( void ) {
-    
+    // PORT initializations
+    DDRA = 0xFF; PORTA = 0x00;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRC = 0x00; PORTC = 0xFF; // other ports are used to display time. Becuase PORTC uses I2C, button inputs are mapped into this port aswell
+    DDRD = 0xFF; PORTD = 0x00;
+
+    // initialize Timer Interrupt
 
 
+    // forever
     while ( true ) {
-	}
+	samplePins();
+	tick_timer();
+	tick_buttons();
+	updateSNC();
+    }
+
+    return 1;
+}
+/////////////////////////////////////////////
+
+void samplePins ( void ) {
+    
+    A0 = ( ~PINC & 0x04 ) >> 2;
+    A1 = ( ~PINC & 0x08 ) >> 3;
+
+    return;
+}
